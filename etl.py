af5a2b0b8572c4793468c2aaf72ee2e6fd8f3c48
Patrick Suzuki <patrickluvsoj@users.noreply.github.com>
2021-02-19 20:49:29 -0800

    Update etl.py

--- etl.py
+++ etl.py
@@ -1,8 +1,7 @@
 from datetime import datetime
 import os
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import udf, col
-from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format
+from pyspark.sql import functions as F
 
 
 os.environ['AWS_ACCESS_KEY_ID']= #empty
